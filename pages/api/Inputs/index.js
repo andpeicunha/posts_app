5d228a1d0e267f35287eaeae88e5530cb6b9596f
André Peixoto <andrepeixoto@gmail.com>
2023-01-20 14:13:17 -0300

    error path componentes

--- pages/api/Inputs/index.js
+++ pages/api/Inputs/index.js
@@ -1,7 +1,7 @@
 // ingredientes
 
-import connectMongo from "../../../lib/connectMongo";
-import { getUser, postUser, putUser } from "../../../lib/controller";
+import connectMongo from "../../lib/connectMongo";
+import { getUser, postUser, putUser } from "../../lib/controller";
 
 export default async function handler(req, res) {
 	connectMongo();
